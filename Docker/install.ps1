2f3cd8e2854426556ce41fab91aadeab1f32d416
Martin Simecek <msimecek@users.noreply.github.com>
2019-02-12 10:31:20 +0000

    Speech CLI version update

--- Docker/install.ps1
+++ Docker/install.ps1
@@ -7,7 +7,7 @@ $currentDir = (Get-Item -Path ".\" -Verbose).FullName
 
 Write-Host "Downloading Speech CLI for Ubuntu."
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -Uri "https://github.com/msimecek/Azure-Speech-CLI/releases/download/1.1.0/ubuntu-x64-1.1.0.zip" -OutFile "$currentDir/speech-cli.zip"
+Invoke-WebRequest -Uri "https://github.com/msimecek/Azure-Speech-CLI/releases/download/1.2.2/ubuntu-x64-1.2.2.zip" -OutFile "$currentDir/speech-cli.zip"
 
 Write-Host "Unpacking Speech CLI."
 Add-Type -Assembly System.IO.Compression.FileSystem
