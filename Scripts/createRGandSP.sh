04ec0f44c00443e05c64f005c6f4ccd8345c286f
Martin Simecek <msimecek@users.noreply.github.com>
2019-04-04 17:07:00 +0200

    Improved prompts

--- Scripts/createRGandSP.sh
+++ Scripts/createRGandSP.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # First run az login 
-echo Enter Resource Group
+echo Enter Resource Group name
 read RESOURCE_GROUP
-echo enter Location - note not all services in this solution are available in all regions - we recommend westeurope or westus
+echo Enter Resource Group location - note: not all services in this solution are available in all regions - we recommend westeurope or westus
 read LOCATION
 az group create --name $RESOURCE_GROUP --location $LOCATION
 #Create Service Principal
