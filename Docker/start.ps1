14c53dcfd7a7e284852130345113ff0e7033cc70
Martin Simecek <msimecek@users.noreply.github.com>
2019-04-21 20:12:22 +0100

    Added ENV switch "mode" to modify container mode

--- Docker/start.ps1
+++ Docker/start.ps1
@@ -1,2 +1,13 @@
-#/usr/src/process-docker.ps1
-/usr/src/process-prechunked.ps1
+if (($null -eq $env:mode) -or ($env:mode -eq "standard")) {
+    /usr/src/process-docker.ps1
+}
+elseif ($env:mode -eq "prechunked") {
+    /usr/src/process-prechunked.ps1
+}
+elseif ($env:mode -eq "debug") {
+    Write-Host "Started in debug mode."
+    /bin/bash
+}
+else {
+    Write-Error -Message "Invalid mode. Set the 'mode' environmental variable to either 'standard' or 'prechunked'."
+}
